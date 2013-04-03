5417e5fa591373af0a8f6f401a79f86d219099ce
Bryan Klimt <klimt@parse.com>
2013-04-03 01:34:26 -0700

    Add font size control and prettify.

--- beefy_text.js
+++ beefy_text.js
@@ -1,4 +1,8 @@
 
+/*
+ * Font Weight Methods
+ */
+
 var fontWeightValue = function(str, defaultValue) {
   if (str === "normal") {
     return 400;
@@ -9,13 +13,13 @@ var fontWeightValue = function(str, defaultValue) {
   return parseInt(str, 10) || defaultValue;
 };
 
-var elementsWithWeightLessThan = function(minWeight) {
+var elementsWithFontWeightLessThan = function(minWeight) {
   return $("*").filter(function() {
     return fontWeightValue($(this).css("font-weight"), -Infinity) < minWeight;
   });
 };
 
-var elementsWithWeightGreaterThan = function(maxWeight) {
+var elementsWithFontWeightGreaterThan = function(maxWeight) {
   return $("*").filter(function() {
     return fontWeightValue($(this).css("font-weight"), Infinity) > maxWeight;
   });
@@ -31,20 +35,98 @@ var normalizeFontWeight = function(minWeight, maxWeight) {
   // If that doesn't work, overwrite the style attribute with "!important".
 
   if (maxWeight) {
-    elementsWithWeightGreaterThan(maxWeight).css("font-weight", maxWeight);
+    elementsWithFontWeightGreaterThan(maxWeight).css("font-weight", maxWeight);
     var rule = ";font-weight:" + maxWeight + " !important";
-    _.each(elementsWithWeightGreaterThan(maxWeight), function(element) {
+    _.each(elementsWithFontWeightGreaterThan(maxWeight), function(element) {
       $(element).attr("style", $(element).attr("style") + rule);
     });
   }
 
   if (minWeight) {
-    elementsWithWeightLessThan(minWeight).css("font-weight", minWeight);
+    elementsWithFontWeightLessThan(minWeight).css("font-weight", minWeight);
     rule = ";font-weight:" + minWeight + " !important";
-    _.each(elementsWithWeightLessThan(minWeight), function(element) {
+    _.each(elementsWithFontWeightLessThan(minWeight), function(element) {
       $(element).attr("style", $(element).attr("style") + rule);
     });
   }
+};
+
+/*
+ * Font Size Methods
+ */
+
+var fontSizeValue = function(str, defaultValue) {
+  match = /(.*)pt$/.exec(str);
+  if (match) {
+    str = match[1];
+  }
+  match = /(.*)px$/.exec(str);
+  if (match) {
+    str = match[1];
+  }
+  return parseInt(str, 10) || defaultValue;
+};
+
+var elementsWithFontSizeLessThan = function(minSize) {
+  return $("*").filter(function() {
+    if ($(this).css("font-size") === "0px") {
+      return false;
+    }
+    return fontSizeValue($(this).css("font-size"), -Infinity) < minSize;
+  });
+};
+
+var elementsWithFontSizeGreaterThan = function(maxSize) {
+  if ($(this).css("font-size") === "0px") {
+    return false;
+  }
+  return $("*").filter(function() {
+    return fontSizeValue($(this).css("font-size"), Infinity) > maxSize;
+  });
+};
+
+var normalizeFontSize = function(minSize, maxSize) {
+  // Set the default font size.
+  if (minSize || maxSize) {
+    $("body").css("font-size", 18);
+  }
+
+  // First try setting the css for every element.
+  // If that doesn't work, overwrite the style attribute with "!important".
+
+  if (maxSize) {
+    elementsWithFontSizeGreaterThan(maxSize).css("font-size", maxSize);
+    var rule = ";font-size:" + maxSize + " !important";
+    _.each(elementsWithFontSizeGreaterThan(maxSize), function(element) {
+      $(element).attr("style", $(element).attr("style") + rule);
+    });
+  }
+
+  if (minSize) {
+    elementsWithFontSizeLessThan(minSize).css("font-size", minSize);
+    rule = ";font-size:" + minSize + " !important";
+    _.each(elementsWithFontSizeLessThan(minSize), function(element) {
+      $(element).attr("style", $(element).attr("style") + rule);
+    });
+  }
+};
+
+
+/*
+ * Routers
+ */
+
+var process = function(message) {
+  var handled = false;
+  if (message.minFontWeight || message.maxFontWeight) {
+    normalizeFontWeight(message.minFontWeight, message.maxFontWeight);
+    handled = true;
+  }
+  if (message.minFontSize || message.maxFontSize) {
+    normalizeFontSize(message.minFontSize, message.maxFontSize);
+    handled = true;
+  }
+  return handled;
 };
 
 // Listen for DOM messages to rerun the method.
@@ -52,13 +134,12 @@ window.addEventListener("message", function(event) {
   if (event.source !== window) {
     return;
   }
-  normalizeFontWeight(event.data.minFontWeight, event.data.maxFontWeight);
+  process(event.data);
 }, false);
 
 // Listen for Chrome messages to rerun the method.
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.minFontWeight || request.maxFontWeight) {
-    normalizeFontWeight(request.minFontWeight, request.maxFontWeight);
+  if (process(request)) {
     sendResponse({ success: true });
   }
 });
--- popup.js
+++ popup.js
@@ -1,28 +1,64 @@
 $(function() {
 
-var onSlide = function(event, slider) {
-  var min = slider.values[0];
-  var max = slider.values[1];
-  $("#sliderLabel").html("Font Weight Range: [" + min + ", " + max + "]");
-
+var sendMessage = function(message) {
   chrome.tabs.getSelected(null, function(tab) {
-    var message = {
-      minFontWeight: min,
-      maxFontWeight: max
-    };
     chrome.tabs.sendMessage(tab.id, message, function(response) {
       console.log(JSON.stringify(response));
     });
   });
 };
 
-$("#slider").slider({
+var tooltipRetainCount = 0;
+
+var showTooltip = function(x, y, text) {
+  x = x - 5;
+  y = y + 5;
+
+  var tooltip = $("#tooltip");
+  tooltip.html(text);
+  tooltip.css("left", (x - tooltip.width()) + "px");
+  tooltip.css("top", y + "px");
+  tooltip.css("display", "block");
+  tooltipRetainCount++;
+  setTimeout(function() {
+    tooltipRetainCount--;
+    if (tooltipRetainCount === 0) {
+      $("#tooltip").css("display", "none");
+    }
+  }, 1000);
+};
+
+$("#fontWeightSlider").slider({
   range: true,
   min: 100,
   max: 900,
   step: 100,
   values: [100, 900],
-  slide: onSlide
+  slide: function(event, slider) {
+    var min = slider.values[0];
+    var max = slider.values[1];
+    sendMessage({
+      minFontWeight: min,
+      maxFontWeight: max
+    });
+    showTooltip(event.pageX, event.pageY, "[" + min + ", " + max + "]");
+  }
+});
+
+$("#fontSizeSlider").slider({
+  range: true,
+  min: 200,
+  max: 700,
+  values: [200, 700],
+  slide: function(event, slider) {
+    var min = Math.round(Math.pow(2, slider.values[0] / 100.0));
+    var max = Math.round(Math.pow(2, slider.values[1] / 100.0));
+    sendMessage({
+      minFontSize: min,
+      maxFontSize: max
+    });
+    showTooltip(event.pageX, event.pageY, "[" + min + ", " + max + "]");
+  }
 });
 
 });
